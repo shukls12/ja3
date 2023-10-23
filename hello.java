562d002f1af3fbca0493506ee39d54e20fe0026c
Ubuntu <ubuntu@ip-172-31-25-162.eu-north-1.compute.internal>
2023-10-23 15:29:28 +0000

    my first commit

---
+++ hello.java
@@ -0,0 +1,22 @@
+// Main class
+public class GFG {
+
+	// Main driver method 
+	public static void main(String[] args)
+	{
+	     // Demonstrating Set using Hashset 
+	     // Declaring object of type string 
+	     Set<String> Hash_set = new HashSet<String>();
+	     
+	     // Adding elements to the Sets
+	     // using add() method
+	     hash_Set.add("Geeks");
+	     hash_Set.add("for");
+	     hash_Set.add("Geeks");
+	     hash_Set.add("Example");
+	     hash_Set.add("Set");
+
+	     // Printing elements of HashSet object
+	     System.out.println(hash_Set);
+
+	} 
